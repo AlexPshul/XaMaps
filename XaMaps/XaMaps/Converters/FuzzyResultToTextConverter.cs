650cb8a3c80b31ad4e170ef9b327bf455584fdd9
Alex Pshul <alex@pshul.com>
2018-10-13 18:08:40 +0300

    Fixed a few visual issues

---
+++ XaMaps/XaMaps/Converters/FuzzyResultToTextConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using XaMaps.Models;
+
+namespace XaMaps.Converters
+{
+    public class FuzzyResultToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SingleFuzzyResult singleFuzzyResult = value as SingleFuzzyResult;
+            if (singleFuzzyResult == null)
+                return null;
+
+            return !string.IsNullOrEmpty(singleFuzzyResult.Poi?.Name)
+                ? singleFuzzyResult.Poi.Name
+                : singleFuzzyResult.Address?.FreeformAddress;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+    }
+}
